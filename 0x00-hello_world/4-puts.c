7bb226b64362306d4ccb15bd8ceba7125cb8a6e8
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-04-07 18:55:15 -0700

    Created the  Hello Puts script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#includes <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    returns(0);
+}
