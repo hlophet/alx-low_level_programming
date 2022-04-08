ca497e8b25ed9edf78472aa95d64757933f76b47
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-04-07 19:03:59 -0700

    Updated Hello Puts script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#includes <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,5 +8,5 @@
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    returns(0);
+    return(0);
 }
