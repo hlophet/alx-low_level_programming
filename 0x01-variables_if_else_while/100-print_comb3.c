5347e4ea12e2cb11c6bdb7a46e79d79739ffc096
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-04-08 19:11:38 -0700

    Created Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int b, c;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			if (b < c)
+			{
+				putchar(b);
+				putchar(c);
+
+				if (b != '8' || (b == '8' && c != '9'))
+				{	
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
