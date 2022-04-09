bbe455f121ff04fbbfce20d1837f420b142064d1
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-04-08 21:10:32 -0700

    Updated Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  * Description: prints the alphabet in lowercase, and then in uppercase\n
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
