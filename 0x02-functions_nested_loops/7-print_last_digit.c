6d329959366671f10c577f399dad6b1b35656a4c
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-12 23:34:51 +0200

    Created 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * print_last_digit - Entry point
+ * @n: n is an integer
+ * Description: prints the last digit of a number
+ * Return: an integer
+ */
+int print_last_digit(int n)
+{
+int result;
+result = n % 10;
+return result;
+}
