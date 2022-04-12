cc55bc7ce3944dbc9e071f7faf88d64afa8e9774
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-12 23:58:24 +0200

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - entry point
+ * @n1: integer 1
+ * @n2: integer 2
+ * Description: calculates the sum of 2 numbers
+ * Return: sum
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
