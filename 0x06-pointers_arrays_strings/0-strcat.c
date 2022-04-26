378f20585b0a66699dc37bb14a0b6696ec07de4c
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 00:20:57 +0200

    updated 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0;
-
-while (dest[i] != '\0')
-i++;
-
-while (src[j] != '\0')
+while (*dest != '\0')
 {
-dest[i] = src[j];
-i++;
-j++;
+*dest++;
 }
-dest[i] = '\0';
+
+while (*src != '\0')
+{
+*dest = *src;
+*dest++;
+*src++;
+}
+
+*dest = '\0';
 return (dest);
-}
 }
