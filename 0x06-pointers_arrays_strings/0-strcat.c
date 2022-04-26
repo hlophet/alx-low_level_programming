a36921be6a58c1b7e093879a2730ec38184eb17d
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 00:14:26 +0200

    created the 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatinate 2 strings
+ * @dest: resultant string
+ * @src: source string to be appended to the @dest string
+ * Return: a string
+ */
+char *_strcat(char *dest, char *src)
+{
+  while(*dest != '\0')
+    {
+        *dest++;
+    }
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        *dest++;
+        *src++;
+    }
+
+    *dest = '\0';
+    return (dest);
+}
