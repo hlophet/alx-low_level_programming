f181be92cc5fb479a288ccd0ee62a6cb69c7bb21
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 01:59:48 +0200

    created 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compares 2 strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: an integer - if str1 < str2, negative difference
+ *                    - if s1 == s2, 0
+ *                    - if s1 > s2, positive difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, cmpStat = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
+{
+cmpStat = s1[i] - s2[i];
+i++;
+}
+return (cmpStat);
+}
