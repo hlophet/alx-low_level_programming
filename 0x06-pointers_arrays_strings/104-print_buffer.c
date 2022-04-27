66b6c56f9936d741ef2f5fc3e1fd9e9a5a551085
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 04:08:02 +0200

    creayed 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - prints a buffer 10 bytes at a time
+ * @b: buffer to be printed
+ * @size: number of bytes to be printed from the buffer
+ */
+void print_buffer(char *b, int size)
+{
+
+}
