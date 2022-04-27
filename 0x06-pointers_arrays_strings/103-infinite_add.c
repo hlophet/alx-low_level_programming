af44d8aa9c43b86f57ae100ee8cd606d7d11f993
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 04:04:25 +0200

    created 103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * infinite_add - adds 2 numbers
+ * @n1: 1st number
+ * @n2: 2nd number
+ * @sum: sum of the numbers
+ * @sumSize: size of the sum
+ * Return: sum of n1 and n2
+ */
+char *infinite_add(char *n1, char *n2, char *sum, int sumSize)
+{
+	return (sum);
+}
