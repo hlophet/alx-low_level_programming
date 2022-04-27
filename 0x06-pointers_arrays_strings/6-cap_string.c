5392798af360669eeb4c4aa09ebc9c9e2927b665
hlophet <101598332+hlophet@users.noreply.github.com>
2022-04-27 03:36:03 +0200

    created 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a sentence
+ * @str: string to be capitalized
+ * Return: the resultant string
+ */
+char *cap_string(char *str)
+{
+return (str);
+}
