0658fbdeaf5d0eab88b0bebacfad3aaa1ee4846f
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-04-28 17:18:19 -0700

    created 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculates the exponent x to the power of y
+ * @x: base number to raise to y
+ * @y: exponent that x is raised to
+ * return: an integer - exponent of base x raised to y,
+ *                    - if y is 0 then exponent is 1,
+ *                    - if y is negative then exponent is 1
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+else if (y > 0)
+return (x * _pow_recursion(x, y - 1));
+else
+return (-1);
+}
