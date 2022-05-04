660da88f11ec89b91c0b716d6ebfd01823f381c9
hlophet <101598332+hlophet@users.noreply.github.com>
2022-05-04 20:06:14 +0200

    updated 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,28 +4,28 @@
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
- * @argv[]: array of argc size
+ * @argv: array of argc size
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-  int i = 1, diff = 1;
+int i = 1, diff = 1;
 
-  (void) argv;
+(void) argv;
 
-  if (argc < 2)
-  {
-    printf("Error\n");
-  }
-  else
-  {
-    while (i < argc)
-    {
-      diff = diff * atoi(argv[i]);
-      i++;
-    }
-  }
+if (argc < 2)
+{
+printf("Error\n");
+}
+else
+{
+while (i < argc)
+{
+diff = diff * atoi(argv[i]);
+i++;
+}
+}
 
-  printf("%d\n", diff);
-  return (0);
+printf("%d\n", diff);
+return (0);
 }
