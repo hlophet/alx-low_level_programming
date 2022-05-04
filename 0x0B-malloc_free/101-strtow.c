f36b3ae4ca78e00a19c961ec6fe6d971e98574cd
hlophet <101598332+hlophet@users.noreply.github.com>
2022-05-05 01:10:46 +0200

    created 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words
+ * @str: string to be split
+ * Return: pointer - to an array of strings (words)
+ *         or NULL - if str = NULL, str = "", etc
+ */
+char **strtow(char *str)
+{
+	return (str);
+}
