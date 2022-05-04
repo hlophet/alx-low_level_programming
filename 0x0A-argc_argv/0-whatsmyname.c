6188dd9247d77f9c4ccbc3f508ab072baa509e67
Thembinkosi Hlophe <hlophethk@gmail.com>
2022-05-03 18:25:25 -0700

    created main.h | created 0-whatsmyname.c | created the project directory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: numberof command line arguements
+ * @argv: array of size argc
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Reade me file for ALX 0x0A - Agrc and Agrv.
