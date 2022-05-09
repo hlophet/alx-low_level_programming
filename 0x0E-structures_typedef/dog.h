87efddfcb3f50404616bb062081db262e90e0407
hlophet <101598332+hlophet@users.noreply.github.com>
2022-05-10 00:39:15 +0200

    created dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef SRTUCT_DOG_H
+#define STRUCT_DOG_H
+/**
+ * struct dog - new type describing a dog
+ * @name : dog's name
+ * @age  : dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
