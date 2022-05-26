934a6d71bb058f7d9a50e2c8baa3b947aaed452a
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-05-26 03:40:37 +0200

    updated 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,13 +5,13 @@
  * @head : pointer to the head of the list
  * @idx  : index of the list where the new node will be added
  *         (indices start at 0)
- * @n    : number of the new node to be added
+ * @n    : value for the new node to be added
  * Return: pointer to the new node,
  *         or NULL - if function fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new, *copy = *head;
+listint_t *new, *temp = *head;
 unsigned int node;
 
 new = malloc(sizeof(listint_t));
@@ -23,20 +23,21 @@ new->n = n;
 
 if (idx == 0)
 {
-new->next = copy;
+new->next = temp;
 *head = new;
 return (new);
 }
 
 for (node = 0; node < (idx - 1); node++)
 {
-if (copy == NULL || copy->next == NULL)
+if (temp == NULL || temp->next == NULL)
 return (NULL);
 
-copy = copy->next;
+temp = temp->next;
 }
 
-new->next = copy->next;
-copy->next = new;
+new->next = temp->next;
+temp->next = new;
+
 return (new);
 }
