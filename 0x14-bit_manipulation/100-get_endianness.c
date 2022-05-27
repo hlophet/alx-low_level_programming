b5632459e641e3e1cee2883ca108dbe054642a46
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-05-27 03:18:52 +0200

    created 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_endianness - checks for endianness
+ * Return: 0 - if big-endian,
+ *         1 - if little-endian
+ */
+int get_endianness(void)
+{
+int s;
+char *endian;
+
+s = 1;
+endian = (char *)&s;
+
+if (endian)
+  return (1);
+else
+  return (0);
+}
