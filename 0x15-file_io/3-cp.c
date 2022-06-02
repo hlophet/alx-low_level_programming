6c928ef2774bb9eb4f69ce506544c5a9f839da0a
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-06-02 11:25:57 +0200

    created 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main interface - copies the contents of a file to another file
+ * @argc      : number of arguments supplied to the program
+ * @argv      : array of pointers to the arguments
+ * Return     : 0 on success
+ * Description: exit code 97 - if the argument count is incorrect
+ *              exit code 98 - if file_from does not exist or cannot be read
+ *              exit code 99 - if file_to cannot be created or written to
+ *              exit code 100 - f file_to or file_from cannot be closed
+ */
+int main(int argc, char *argv[])
+{
+
+}
