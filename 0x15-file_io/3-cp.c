58470bf639c2913db6d4e534b66489c9f8fd9268
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-06-02 19:36:29 +0200

    updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,58 +14,58 @@ char *create_buffer(char *);
 
 int main(int argc, char *argv[])
 {
-    int from, to, count, r, w;
-    char *buff;
+int from, to, count, r, w;
+char *buff;
 
-    count = 1024;
+count = 1024;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 
-    buff = create_buffer(argv[2]);
-    from = open(argv[1], O_RDONLY);
-    to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+buff = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    if (from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    else if (to == -1)
-    {
-	    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	    exit(99);
-    }
-    else
-    {
-        while (count == 1024)
-        {
-            r = read(from, buff, 1024);
-            w = write(to, buff, r);
-            
-            if (r == -1)
-            {
-	            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	            exit(98);
-            }
-            else if (w != r)
-            {
-	            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	            exit(99);
-            }
+if (from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+else if (to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+else
+{
+while (count == 1024)
+{
+r = read(from, buff, 1024);
+w = write(to, buff, r);
 
-            count--;
-        }
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+else if (w != r)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
 
-    }
+count--;
+}
 
-    close_file(from);
-    close_file(to);
-    free(buff);
-    return (0);
+}
+
+close_file(from);
+close_file(to);
+free(buff);
+return (0);
 }
 
 /**
@@ -74,11 +74,11 @@ int main(int argc, char *argv[])
  */
 void close_file(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
 }
 
 /**
@@ -88,15 +88,15 @@ void close_file(int fd)
  */
 char *create_buffer(char *filename)
 {
-    char *buff;
+char *buff;
 
-    buff = malloc(1024 * sizeof(char));
+buff = malloc(1024 * sizeof(char));
 
-    if (buff == NULL)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	exit(99);
-    }
+if (buff == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
 
-    return (buff);
+return (buff);
 }
