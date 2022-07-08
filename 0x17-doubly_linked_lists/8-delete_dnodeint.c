b4bebc3c0c5d23d7b967abc3f25571f332390705
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-07-08 05:52:22 +0200

    created 8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes a node from a doubly-liked
+ *                            at a given index
+ * @head : pointer to the head of the list
+ * @index: index of the node to be deleted
+ *
+ * Return: 1 - upon success
+ *        -1 - if function fails
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *tmp = *head;
+
+if (*head == NULL)
+return (-1);
+
+for (; index != 0; index--)
+{		
+if (tmp == NULL)
+return (-1);
+
+tmp = tmp->next;
+}
+
+if (tmp == *head)
+{
+*head = tmp->next;
+
+if (*head != NULL)
+(*head)->prev = NULL;
+}
+else
+{
+tmp->prev->next = tmp->next;
+
+if (tmp->next != NULL)
+tmp->next->prev = tmp->prev;
+}
+
+free(tmp);
+return (1);
+}
