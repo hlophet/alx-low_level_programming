28340a1ef586ebc670e33b6caf1083b72fedff6b
Thembinkosi Hlophe <101598332+hlophet@users.noreply.github.com>
2022-07-08 05:06:40 +0200

    created 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - counts number of elements
+ *                in a linked list
+ * @h    : head of the list
+ * Return: number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nodes = 0;
+
+while (h)
+{
+nodes++;
+h = h->next;
+}
+
+return (nodes);
+}
